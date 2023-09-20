08df8a080333b9546eeb23aaaa7a8828b5a0ea28
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-20 11:15:03 +0530

    Added route for router using path and element

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const myCreatedRoute = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>You are now in home route</div>
+  },
+  {
+    path: '/products',
+    element: <div>You want to see products</div>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <RouterProvider router={myCreatedRoute}></RouterProvider>
   </React.StrictMode>,
 )

684f86ed82b923e8b8e9b1a1405ee5840dbf3e60
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-20 12:16:41 +0530

    For fixed navbar we use nested router to fixed parent and change the outlet/children only

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,55 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
+// const myCreatedRoute = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <div>You are now in home route</div>
+//   },
+//   {
+//     path: '/products',
+//     element: <div>You want to see products</div>
+//   },
+//   {
+//     path: '/about',
+//     element: <div>What do ypu want to know about me?</div>
+//   },
+//   {
+//     path: '/dashboard',
+//     element: <div>This is your dashboard</div>
+//   }
+// ])
+
+// Creating nested router by adding another property named children
 const myCreatedRoute = createBrowserRouter([
   {
-    path: '/',
-    element: <div>You are now in home route</div>
-  },
-  {
-    path: '/products',
-    element: <div>You want to see products</div>
+    path: "/",
+    element: <section>
+      <div>This is a fixed item</div>
+      <Outlet></Outlet>
+    </section>,
+    //children will also have array of objects like createBrowserRouter does
+    //children is known as outlet
+    children: [
+      {
+        path: "/",
+        element: <div>This is home route</div>
+      },
+      {
+        path: '/products',
+        element: <div>You want to see products</div>
+      },
+      {
+        path: '/about',
+        element: <div>What do ypu want to know about me?</div>
+      },
+      {
+        path: '/dashboard',
+        element: <div>This is your dashboard</div>
+      }
+    ]
   }
 ])
 
